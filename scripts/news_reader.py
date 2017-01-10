7ef28263f2d1cb788198170685912cd7e5b82c41
Arun Ghosh <arunghosh@gmail.com>
2017-01-10 12:05:32 +0530

    added scripts

---
+++ scripts/news_reader.py
@@ -0,0 +1,22 @@
+from newspaper import Article
+import requests
+import json
+
+url = "http://www.thehindubusinessline.com/economy/reforms-policy-effectiveness-to-decide-indias-rating-moodys/article9469912.ece?homepage=true"
+article = Article(url)
+article.download()
+print(article.html)
+# cnn_paper = newspaper.build('http://cnn.com')
+# article = cnn_paper.articles[0]
+article.parse()
+article.nlp()
+
+data = {
+    'summary': article.summary,
+    'keywords': article.keywords,
+    'image': article.top_image,
+    'url': article.url,
+    'title': article.title
+}
+print(data)
+requests.post('http://localhost:5000/api/news/', data, headers = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"})
